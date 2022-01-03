6adfa4dadc8dd8185a8d08916f128352bd17932f
17Moons <92429890+17Moons@users.noreply.github.com>
2022-01-03 12:34:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+
+var randomNumber1 = Math.floor(Math.random() * 3) + 1;
+
+var randomImageSource = "images/g" + randomNumber1 + ".png";
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 3) + 1;
+
+var randomImageSource2 = "images/g" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+
+if (randomNumber1 ==1 && randomNumber2 ==2) {
+  document.querySelector("h1").innerHTML = "Play 2 Wins!";
+}
+else if (randomNumber1 ==2 && randomNumber2 == 1) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
+else if (randomNumber1 ==1 && randomNumber2 == 3) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
+else if (randomNumber1 ==3 && randomNumber2 == 1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
+else if (randomNumber1 ==2 && randomNumber2 == 3) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
+else if (randomNumber1 ==3 && randomNumber2 == 2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
+else
+{
+  document.querySelector("h1").innerHTML = "Draw!";
+
+}
+
+function refreshPage(){
+    window.location.reload();
+} 
